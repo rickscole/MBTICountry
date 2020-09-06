6cb08ef23bbcb79e09e88d15afe77042bf35e939
rickscole <ricksaubercole@gmail.com>
2020-09-06 00:53:55 -0400

    Add files via upload

--- Create MBTI VW_Similarity_HOFF.sql
+++ Create MBTI VW_Similarity_HOFF.sql
@@ -1,12 +1,15 @@
 USE [MBTI]
 GO
 
-/****** Object:  View [MBTI].[VW_Similarity_HOFF]    Script Date: 8/26/2020 8:06:11 PM ******/
+/****** Object:  View [MBTI].[VW_Similarity_HOFF]    Script Date: 9/6/2020 12:52:36 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+
+
+
 
 
 
@@ -24,8 +27,8 @@ GO
 
 CREATE VIEW [MBTI].[VW_Similarity_HOFF] AS
 SELECT 
-tSim.*,
-tCountry.[Country_Adjusted]
+tSim.*
+, tCountry.[SetCountryName] AS [Country_Adjusted]
 --, tIter.[Introversion] AS [Introversion_User]
 --, tIter.[Intuition] AS [Intuition_User]
 --, tIter.[Thinking] AS [Thinking_User]
@@ -37,21 +40,12 @@ tCountry.[Country_Adjusted]
 --, tIter.[Prospecting] AS [Prospecting_User]
 --, tIter.[Assertiveness] AS [Assertiveness_User]
 FROM [MBTI].[MBTI].[TBL_Similarity_HOFF] tSim
-INNER JOIN
-(
-	SELECT 
-	DISTINCT [Country]
-	, CASE WHEN [Country] = 'U.S.A.' THEN 'United States' ELSE [Country] END AS [Country_Adjusted]
-	FROM [MBTI].[MBTI].[TBL_Similarity_HOFF]
-) 
-tCountry
-ON tSim.[Country] = tCountry.[Country]
+INNER JOIN [MBTI].[MBTI].[TBL_CountryTranslations] tCountry
+ON tSim.[Country] = tCountry.[GetCountryName]
 --INNER JOIN  [MBTI].[MBTI].[TBL_Iteration_MBTI] tIter
 --ON tSim.[FK_ID_Iteration] = tIter.[PK_ID_Iteration_CountrySimilarity_MBTI]
 WHERE tSim.[FK_ID_Iteration] = (SELECT MAX([FK_ID_Iteration]) FROM [MBTI].[MBTI].[TBL_Similarity_HOFF])
-
---GO
-
+AND tCountry.[SetCountryName] NOT IN ('Africa East','Africa West','Arab countries')
 
 GO
 
