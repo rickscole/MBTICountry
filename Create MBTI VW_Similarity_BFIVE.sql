c249f334873830dc0e9368e8f2bb4d5f74fee6c7
rickscole <ricksaubercole@gmail.com>
2020-09-06 00:52:08 -0400

    Add uniform country name
    Inner join TBL_CountryTranslations to get uniform country name

--- Create MBTI VW_Similarity_BFIVE.sql
+++ Create MBTI VW_Similarity_BFIVE.sql
@@ -1,7 +1,7 @@
 USE [MBTI]
 GO
 
-/****** Object:  View [MBTI].[VW_Similarity_BFIVE]    Script Date: 8/26/2020 8:07:00 PM ******/
+/****** Object:  View [MBTI].[VW_Similarity_BFIVE]    Script Date: 9/6/2020 12:47:30 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,24 +9,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-
-
-
-
-/****** Object:  View [MBTI].[VW_Similarity_MBTI]    Script Date: 8/21/2020 1:39:49 PM ******/
---SET ANSI_NULLS ON
---GO
-
---SET QUOTED_IDENTIFIER ON
---GO
-
-
-
-
 CREATE VIEW [MBTI].[VW_Similarity_BFIVE] AS
 SELECT 
-tSim.*,
-tCountry.[Country_Adjusted]
+tSim.*
+, tCountry.[SetCountryName] AS [Country_Adjusted]
 --, tIter.[Introversion] AS [Introversion_User]
 --, tIter.[Intuition] AS [Intuition_User]
 --, tIter.[Thinking] AS [Thinking_User]
@@ -37,21 +23,12 @@ tCountry.[Country_Adjusted]
 --, tIter.[Thinking] AS [Thinking_User]
 --, tIter.[Prospecting] AS [Prospecting_User]
 --, tIter.[Assertiveness] AS [Assertiveness_User]
-FROM [MBTI].[MBTI].[TBL_Similarity_BFIVE] tSim
-INNER JOIN
-(
-	SELECT 
-	DISTINCT [Country]
-	, CASE WHEN [Country] = 'U.S.A.' THEN 'United States' ELSE [Country] END AS [Country_Adjusted]
-	FROM [MBTI].[MBTI].[TBL_Similarity_BFIVE]
-) 
-tCountry
-ON tSim.[Country] = tCountry.[Country]
---INNER JOIN  [MBTI].[MBTI].[TBL_Iteration_MBTI] tIter
---ON tSim.[FK_ID_Iteration] = tIter.[PK_ID_Iteration_CountrySimilarity_MBTI]
+FROM [MBTI].[MBTI].[TBL_Similarity_BFIVE] AS tSim
+INNER JOIN [MBTI].[MBTI].[TBL_CountryTranslations] tCountry
+ON tSim.[Country] = tCountry.[GetCountryName]
+--INNER JOIN  [MBTI].[MBTI].[TBL_Iteration_BFIVE] tIter
+--ON tSim.[FK_ID_Iteration] = tIter.[PK_ID_Iteration_CountrySimilarity_BFIVE]
 WHERE tSim.[FK_ID_Iteration] = (SELECT MAX([FK_ID_Iteration]) FROM [MBTI].[MBTI].[TBL_Similarity_BFIVE])
-
---GO
 
 
 GO
