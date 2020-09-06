006ae49d3e172130401c5c234b369fdd30c4432c
rickscole <ricksaubercole@gmail.com>
2020-09-06 00:58:41 -0400

    Add uniform country name
    Inner join TBL_CountryTranslations to get uniform country name

--- Create MBTI VW_Similarity_MBTI.sql
+++ Create MBTI VW_Similarity_MBTI.sql
@@ -1,7 +1,7 @@
 USE [MBTI]
 GO
 
-/****** Object:  View [MBTI].[VW_Similarity_MBTI]    Script Date: 8/20/2020 11:38:29 AM ******/
+/****** Object:  View [MBTI].[VW_Similarity_MBTI]    Script Date: 9/6/2020 12:57:07 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,12 +9,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-
-
 CREATE VIEW [MBTI].[VW_Similarity_MBTI] AS
 SELECT 
 tSim.*
-, tCountry.[CountryName_Adjusted]
+, tCountry.[SetCountryName] AS [Country_Adjusted]
 --, tIter.[Introversion] AS [Introversion_User]
 --, tIter.[Intuition] AS [Intuition_User]
 --, tIter.[Thinking] AS [Thinking_User]
@@ -25,18 +23,13 @@ tSim.*
 --, tIter.[Thinking] AS [Thinking_User]
 --, tIter.[Prospecting] AS [Prospecting_User]
 --, tIter.[Assertiveness] AS [Assertiveness_User]
-FROM [MBTI].[MBTI].[TBL_Similarity_MBTI] tSim
-INNER JOIN
-(
-	SELECT 
-	DISTINCT REPLACE(UPPER(LEFT([Country],1)) + LOWER(SUBSTRING([Country],2,LEN([Country]))),'-',' ') AS [CountryName_Adjusted]
-	, [Country] 
-	FROM [MBTI].[MBTI].[TBL_Similarity_MBTI]
-) 
-tCountry
-ON tSim.[Country] = tCountry.[Country]
+FROM [MBTI].[MBTI].[TBL_Similarity_MBTI] AS tSim
+INNER JOIN [MBTI].[MBTI].[TBL_CountryTranslations] tCountry
+ON tSim.[Country] = tCountry.[GetCountryName]
 --INNER JOIN  [MBTI].[MBTI].[TBL_Iteration_MBTI] tIter
 --ON tSim.[FK_ID_Iteration] = tIter.[PK_ID_Iteration_CountrySimilarity_MBTI]
+WHERE tSim.[FK_ID_Iteration] = (SELECT MAX([FK_ID_Iteration]) FROM [MBTI].[MBTI].[TBL_Similarity_MBTI])
+
 
 GO
 
